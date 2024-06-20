88ec54a93d5f697dabf569b98914b29c0dd6248e
Farid Muhammad S <faridsopian08@gmail.com>
2024-06-20 15:38:23 +0700

    Mempercantik tampilan

---
+++ app/src/main/java/com/bangkitacademy/agrosense/view/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.bangkitacademy.agrosense.view.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bangkitacademy.agrosense.R
+import com.bangkitacademy.agrosense.view.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
